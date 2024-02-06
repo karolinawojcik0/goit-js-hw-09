acacd75b3cba8d324a582fbaa9b0a0cdde168868
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-02-06 22:01:10 +0100

    task three

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
   }
-}
+
+  document.getElementById('createPromisesForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const firstDelay = parseInt(formData.get('delay'));
+    const step = parseInt(formData.get('step'));
+    const amount = parseInt(formData.get('amount'));
+
+    for (let i = 0; i < amount; i++) {
+      const currentDelay = firstDelay + i * step;
+
+      createPromise(i + 1, currentDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  });
